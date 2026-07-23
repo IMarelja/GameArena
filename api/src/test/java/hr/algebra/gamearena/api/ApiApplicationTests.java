a907857c07e8e6e955782791bd84b3feaa0b72af
IMarelja <imarelja@algebra.hr>
2026-07-23 15:27:55 +0200

    init

---
+++ api/src/test/java/hr/algebra/gamearena/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.gamearena.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ webapp/src/test/java/hr/algebra/gamearena/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.gamearena.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
