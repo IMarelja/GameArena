426684d01c76b8d976b5cebcc72d14e45834f21e
IMarelja <imarelja@algebra.hr>
2026-07-23 17:43:13 +0200

    Blank rest controllers

---
+++ api/src/main/java/hr/algebra/gamearena/api/rest_controller/AuthenticationController.java
@@ -0,0 +1,9 @@
+package hr.algebra.gamearena.api.rest_controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/auth")
+public class AuthenticationController {
+
+
+}
---
+++ api/src/main/java/hr/algebra/gamearena/api/rest_controller/UserController.java
@@ -0,0 +1,7 @@
+package hr.algebra.gamearena.api.rest_controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/user")
+public class UserController {
+}
