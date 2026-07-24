909bb635195fd61a6dbf8d0919a55a081bbfe878
IMarelja <imarelja@algebra.hr>
2026-07-24 17:00:50 +0200

    Async app

--- api/src/main/java/hr/algebra/gamearena/api/ApiApplication.java
+++ api/src/main/java/hr/algebra/gamearena/api/ApiApplication.java
@@ -2,7 +2,9 @@ package hr.algebra.gamearena.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
 
+@Async
 @SpringBootApplication
 public class ApiApplication {
 
