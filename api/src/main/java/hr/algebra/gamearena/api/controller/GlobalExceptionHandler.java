9b5f8d6e01b0cd7ca95c23d73d47b6d5f3e64c6a
IMarelja <imarelja@algebra.hr>
2026-07-26 11:37:34 +0200

    GlobalExceptionHandler for generic RuntimeExceptions and generic Exception

--- api/src/main/java/hr/algebra/gamearena/api/controller/GlobalExceptionHandler.java
+++ api/src/main/java/hr/algebra/gamearena/api/controller/GlobalExceptionHandler.java
@@ -2,18 +2,24 @@ package hr.algebra.gamearena.api.controller;
 
 import hr.algebra.gamearena.api.dto.other.ApiError;
 import hr.algebra.gamearena.api.dto.other.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.yaml.snakeyaml.nodes.Tag.STR;
-
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(RuntimeException ex) {
+        log.error("Undefined RuntimeException error: {}", ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error(new ApiError("Undefined RuntimeException error: " + ex.getMessage())));
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception ex) {
-        return ResponseEntity.status(500).body(ApiResponse.error(new ApiError("Internal server error: " + ex.getMessage())));
+        log.error("Undefined Exception error: {}", ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error(new ApiError("Undefined Exception error: " + ex.getMessage())));
     }
 }
