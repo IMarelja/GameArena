6a7e518e4521334af3f5a81256d18eea4c77893b
IMarelja <imarelja@algebra.hr>
2026-08-19 11:19:34 +0200

    Minor enum fixes

--- api/src/main/java/hr/algebra/gamearena/api/dto/team/invitation/InviterTeamInvitationEditRequest.java
+++ api/src/main/java/hr/algebra/gamearena/api/dto/team/invitation/InviterTeamInvitationEditRequest.java
@@ -1,5 +1,6 @@
 package hr.algebra.gamearena.api.dto.team.invitation;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class InviterTeamInvitationEditRequest {
+    @NotNull(message = "Status is required")
     private InviterInviteStatus status;
 }
